185947781ead18164dedae74c1b5c8597dddc815
Vishal Sharma <outreach.vishal@gmail.com>
2025-08-11 19:46:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 5/pattern-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+
+    void printTriangle(int n) {
+        // code here
+        for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+    {
+        cout << "* ";  
+    }
+    cout << endl;
+    }
+    }
+};
